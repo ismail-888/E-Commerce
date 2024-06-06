d9acbc788f7db0e8323975b1265876be8e4c2733
ismail-888 <67410690+ismail-888@users.noreply.github.com>
2024-06-06 17:57:01 +0300

    base changes

---
+++ client/src/pages/Login.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import loginIcons from "../assest/signin.gif";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router-dom";
+const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section id="login">
+      <div className="mx-auto container p-4">
+        <div className="bg-white p-5 w-full max-w-sm mx-auto">
+          <div className="w-20 h-20 mx-auto">
+            <img src={loginIcons} alt="Login icons" />
+          </div>
+
+          <form className="pt-6" onSubmit={handleSubmit}>
+            <div className="grid">
+              <label>Email :</label>
+              <div className="bg-slate-100 p-2 ">
+                <input
+                  onChange={handleOnChange}
+                  name="email"
+                  value={data.email}
+                  type="email"
+                  placeholder="enter email"
+                  className="w-full h-full outline-none bg-transparent"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label>Password :</label>
+              <div className="bg-slate-100 p-2 flex items-center">
+                <input
+                  onChange={handleOnChange}
+                  name="password"
+                  value={data.password}
+                  type={showPassword ? "text" : "password"}
+                  placeholder="enter password"
+                  className="w-full h-full outline-none bg-transparent"
+                />
+                <div
+                  className="cursor-pointer text-xl"
+                  onClick={() => setShowPassword((prev) => !prev)}
+                >
+                  <span>{showPassword ? <FaEyeSlash /> : <FaEye />}</span>
+                </div>
+              </div>
+              <Link
+                to={"/forgot-password"}
+                className="block w-fit ml-auto hover:underline hover:text-red-600"
+              >
+                Forgot password ?
+              </Link>
+            </div>
+
+            <button className="bg-red-600 hover:bg-red-700 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6">
+              Login
+            </button>
+          </form>
+          <p className="my-5">
+            Don't have account ?{" "}
+            <Link
+              to={"/sign-up"}
+              className="text-red-600 hover:underline hover:text-red-700"
+            >
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
