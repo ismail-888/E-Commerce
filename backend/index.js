54d91fced1130d9e372301c4ff8bc8ea98055fdc
ismail-888 <67410690+ismail-888@users.noreply.github.com>
2024-07-08 20:41:01 +0300

    add vercel config file

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,6 @@ const cookieParser=require('cookie-parser')
 require("dotenv").config();
 const connectDB = require('./config/db.js')
 const router=require('./routes/index.js')
-const path =require('path')
-
-
-// publish
-const __direname=path.resolve();
 
 const app = express();
 app.use(cors({
@@ -23,11 +18,7 @@ app.use("/api",router)
 
 const PORT = 8080 || process.env.PORT;
 
-//publish
-app.use(express.static(path.join(__direname,'/client/build')));
-app.get('*',(req,res)=>{
-  res.sendFile(path.join(__direname,'client','build','index.html'))
-})
+
 
 
 connectDB().then(()=>{
